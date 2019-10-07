a3ccddd807b62d9fe77f87a9a7637a4b82c3cdad
Giordano Rodrigues <22085029+giordanorn@users.noreply.github.com>
2019-10-07 03:24:44 -0300

    finish Funktor.hs

--- Funktor.hs
+++ Funktor.hs
@@ -13,15 +13,33 @@ instance Funktor [] where
     fmap = map
 
 instance Funktor Maybe where
-    fmap = undefined
+    -- fmap :: (a -> b) -> Maybe a -> Maybe b
+    fmap f Nothing  = Nothing
+    fmap f (Just x) = Just (f x)
 
 -- what about Either?
+instance Funktor (Either c) where
+    -- fmap :: (a -> b) -> Either c a -> Either c b
+    fmap f (Left x)  = Left x
+    fmap f (Right x) = Right (f x)
 
 -- what about pairs?
+instance Funktor ((,) c) where
+    -- fmap :: (a -> b) -> (c,a) -> (c,b)
+    fmap f (x,y)  = (x,f y)
 
 -- what about functions?
+instance Funktor ((->) c) where
+    -- fmap :: (a -> b) -> (c -> a) -> (c -> b)
+    fmap = (.)
 
 -- what about Trees?
+data Tree a = Leaf a | Node a (Tree a) (Tree a)
+    deriving (Show, Eq)
+
+instance Funktor Tree where
+    -- fmap :: (a -> b) -> Tree a -> Tree b
+    fmap f (Leaf x)     = Leaf (f x)
+    fmap f (Node x t u) = Node (f x) (fmap f t) (fmap f u)
 
 -- ...define Functor instances of as many * -> * things as you can think of!
-
